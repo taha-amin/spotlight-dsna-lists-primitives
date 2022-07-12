629aac25faac71173e45eef7bd6d512f6c6eb5da
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-07-12 10:48:26 -0700

    got first test passing. Working on second test

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 function reverseWords(sentence) {
-    //split the sentence
-    //map over the split sentence 
-    //reverse the arr 
-    //join it all 
-    let reversedSentence = sentence.split(" ")
-    .map(word => word.split("")
-    .reverse()
-    .join(""));
-    return reversedSentence.join(" ");
-    
+  //split the sentence
+  //map over the split sentence
+  //reverse the arr
+  //join it all
+  let reversedSentence = sentence
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""));
+  return reversedSentence.join(" ");
 }
 
+function titleCaseWords(sentence) {
+  //make sentence all lowercase
+  //split sentence
+  //map through each word
+  //use charAt for first letter and make that uppercase
+  //join sentence back together
+  sentence
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
 
-module.exports = { reverseWords };
+module.exports = { reverseWords, titleCaseWords };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,13 @@
-const { reverseWords } = require(".");
+const { reverseWords, titleCaseWords } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
+  });
+});
+
+describe("title case words", () => {
+  it("should uppercase the first letter in every word of a sentence", () => {
+    expect(titleCaseWords("alchemy ROCKS goLD")).toBe("Alchemy Rocks Gold");
   });
 });
