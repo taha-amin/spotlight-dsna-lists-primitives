128ff053d476202d30d2090de2d50d363125e00e
Khayman King <khaymanak@gmail.com>
2022-07-12 10:21:49 -0700

    able to get reversed words function to pass test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function reverseWords(sentence) {}
+function reverseWords(sentence) {
+    //split the sentence
+    //map over the split sentence 
+    //reverse the arr 
+    //join it all 
+    let reversedSentence = sentence.split(" ")
+    .map(word => word.split("")
+    .reverse()
+    .join(""));
+    return reversedSentence.join(" ");
+    
+}
+
 
 module.exports = { reverseWords };
